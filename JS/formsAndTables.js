5e25f75aec035d6dfbfb88955bd9f150d1d9b730
Emmanuel <emmanueltechrd@gmail.com>
2026-02-16 18:05:12 -0400

    [UPDATE] Learning more about buttons

--- JS/formsAndTables.js
+++ JS/formsAndTables.js
@@ -1,2 +1,16 @@
+
+// Button class "alert-btn"
 const btn = document.querySelector(".alert-btn");
 btn.addEventListener("click", () => alert("This is a JavaScript programmed alert"));
+
+// Button class "start-btn"
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".start-btn");
+  btn.addEventListener("click", () => {
+    const paraEl = document.createElement("p");
+    const bodyEl = document.querySelector("body");
+
+    bodyEl.appendChild(paraEl);
+    paraEl.textContent = "The game has started!!!";
+  });
+});
