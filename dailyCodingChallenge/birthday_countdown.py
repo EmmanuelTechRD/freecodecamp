ca6b98f8ab4afb3b66c40262a29feda1ecad0f68
Emmanuel <emmanueltechrd@gmail.com>
2026-07-17 17:59:18 -0400

    [ADD] birthday_countdown python function july 17

---
+++ dailyCodingChallenge/birthday_countdown.py
@@ -0,0 +1,32 @@
+import datetime
+
+def days_until_birthday(today, birthday):
+
+    # parsing today's date (expects "YYYY-MM-DD")
+    today = datetime.date.fromisoformat(today)
+
+    # parsing the birthday month and day (expects "M/D")
+    b_month, b_day = map(int, birthday.split('/'))
+
+    # start checking from the current year
+    test_year = today.year
+
+    while True:
+        try:
+            # creating a valid date for the birthday in the test_year
+            b_date = datetime.date(test_year, b_month, b_day)
+
+            # the next birthday must be strictly in the future
+            if b_date > today:
+                return (b_date - today).days
+            else:
+                test_year += 1
+            
+        except ValueError:
+            # this handles Feb 29 birthdays on non-leap years.
+            # datetime.date() throws a ValueError, so we safely skip to the next year.
+            pass
+        
+            test_year += 1
+
+print(days_until_birthday("2026-07-16", "9/7"))
